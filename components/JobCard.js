fd9a67653fc389ab8c4ae460f62026940548c9f1
Haiyang Hong <hong961127@gmail.com>
2020-11-04 21:26:58 -0800

    Made JobCard responsive

--- components/JobCard.js
+++ components/JobCard.js
@@ -1,10 +1,21 @@
+import clsx from "clsx";
+
 const JobCard = ({ job }) => {
   return (
-    <div className="flex w-9/12 items-center justify-between p-6 bg-white mb-4 rounded-lg shadow-lg">
+    <div
+      className={clsx(
+        "relative flex flex-col mb-10 mx-10 items-start pt-8 md:flex-row md:items-center justify-between p-6 md:pt-6 bg-white md:mb-4 rounded-md shadow-lg",
+        { ["border-l-4 border-primary"]: job.featured }
+      )}
+    >
       {/* Left */}
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center md:space-x-4">
         {/* Company Logo */}
-        <img src={job.logo} alt={job.company} className="block w-16 h-16" />
+        <img
+          src={job.logo}
+          alt={job.company}
+          className="absolute top-0 -mt-6 w-12 h-12 md:mt-0 md:static md:block md:w-16 md:h-16"
+        />
 
         {/* Job Information */}
         <div className="flex flex-col space-y-2">
@@ -12,11 +23,13 @@ const JobCard = ({ job }) => {
           <div className="flex items-center">
             <h3 className="mr-4 text-xs font-semibold text-primary">
               {job.company}
+
               {job.new && (
                 <span className="ml-3 mr-2 text-xxs uppercase text-white bg-primary p-2 rounded-full">
                   New!
                 </span>
               )}
+
               {job.featured && (
                 <span className="text-xxs uppercase text-white bg-dark-cyan-2 p-2 rounded-full">
                   Featured!
@@ -26,45 +39,51 @@ const JobCard = ({ job }) => {
           </div>
 
           {/* Job position */}
-          <h2 className="text-gray-900 font-semibold">{job.position}</h2>
+          <h2 className="text-gray-900 font-bold cursor-pointer hover:text-primary">
+            {job.position}
+          </h2>
 
           {/* Job posted at, contract, and location */}
           <div className="flex items-center space-x-2">
-            <span className="text-gray-500 text-xs font-medium">
+            <span className="text-gray-600 text-xs font-medium">
               {job.contract}
             </span>
-            <span className="text-gray-500 text-xs font-medium">•</span>
-            <span className="text-gray-500 text-xs font-medium">
+            <span className="text-gray-600 text-xs font-medium">•</span>
+            <span className="text-gray-600 text-xs font-medium">
               {job.postedAt}
             </span>
-            <span className="text-gray-500 text-xs font-medium">•</span>
-            <span className="text-gray-500 text-xs font-medium">
+            <span className="text-gray-600 text-xs font-medium">•</span>
+            <span className="text-gray-600 text-xs font-medium">
               {job.location}
             </span>
           </div>
         </div>
       </div>
 
+      <hr className="md:hidden w-full bg-gray-200 my-2" />
+
       {/* Right - Job meta info */}
-      <div className="flex items-center space-x-4">
-        <span className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded">
+      <div className="flex flex-wrap justify-start items-center cursor-pointer space-y-2">
+        <span className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded hover:bg-primary hover:text-white mr-2 last:mr-0">
           {job.role}
         </span>
-        <span className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded">
+        <span className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded hover:bg-primary hover:text-white mr-2 last:mr-0">
           {job.level}
         </span>
-        {job.languages.map((lang, idx) => (
+
+        {job.languages?.map((lang, idx) => (
           <span
             key={idx}
-            className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded"
+            className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded hover:bg-primary hover:text-white mr-2 last:mr-0"
           >
             {lang}
           </span>
         ))}
-        {job.tools.map((tool, idx) => (
+
+        {job.tools?.map((tool, idx) => (
           <span
             key={idx}
-            className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded"
+            className="text-xs font-semibold p-2 bg-light-cyan text-primary rounded hover:bg-primary hover:text-white mr-2 last:mr-0"
           >
             {tool}
           </span>
